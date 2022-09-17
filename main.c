c94561d6f5ae020326dd282cf3c160ff29867200
saad moumou <saad.moumou.coder@gmail.com>
2022-09-17 23:04:06 +0100

    FIX SOME BUH

--- main.c
+++ main.c
@@ -13,14 +13,15 @@ struct produit{
 
 //OperationProduits
 void OperationProduits(struct produit p[100]){
-    int N=0,OP;
+    int N,OP;
     do{
         printf("Choisir une operation:\n");
         printf("1-Ajouter Un Produit\n");
         printf("2-Modifier Un Produit\n");
         printf("3-Supprimer Un Produit\n");
         printf("4-Afficher Les Produits\n");
-        printf("5-Quitter De Programme\n");
+        printf("5-Trier Un Produit\n");
+        printf("6-Quitter De Programme\n");
         scanf("%d",&OP);
 
 
@@ -28,25 +29,28 @@ void OperationProduits(struct produit p[100]){
             case 1:
                 printf("Combien de produits souhaitez-vous saisir ?: ");
                 scanf("%d",&N);
-                ajouterPreduits(p,N);
+                ajouterProduits(p,N);
                 break;
             case 2:
                 break;
             case 3:
                 break;
             case 4:
-                afficherPreduit(p,N);
+                afficherProduit(p,N);
                 break;
             case 5:
+                OperationTrier(p);
+                break;
+            case 6:
                 printf("By by\n");
                 break;
         }
 
-    }while(OP != 5);
+    }while(OP != 6);
 
 }
 //Function ajouter
-void ajouterPreduits(struct produit p[100],int N){
+void ajouterProduits(struct produit p[100],int N){
     //ajouter
     for(int i=0; i<N; i++){
         printf("\nDonnez Le Code Produit: ");
@@ -61,7 +65,7 @@ void ajouterPreduits(struct produit p[100],int N){
     }
 }
 //afficherPreduit
-void afficherPreduit(struct produit p[100],int N){
+void afficherProduit(struct produit p[100],int N){
     //afficher
     for(int j=0; j<N; j++){
         printf("Produit %d:\nCode: %d\nNom: %s\nQuantitie: %d\nPrix: %.2f\n",j+1,p[j].code,p[j].nom,p[j].quantite,p[j].prix);
@@ -74,7 +78,7 @@ void orderAlpha(struct produit p[100],int count){
     for(i=0; i<count-1; i++){
         for(j=i+1; j<count; j++){
             if(strcmp(p[i].nom,p[j].nom) > 0){ //if p[i].nom > p[j].nom So strcmp return value positive
-                X=p[i];
+                X = p[i];
                 p[i] = p[j];
                 p[j]= X;
             }
@@ -96,6 +100,24 @@ void orderPrix(struct produit p[100],int count){
     }
 }
 
+void OperationTrier(struct produit p[100]){
+    int op;
+    int count = 2;
+    do{
+    printf("1-L’ordre Alphabetique Croissant Du Nom:\n");
+    printf("2-L’ordre Decroissant Du Prix:");
+    scanf("%d",&op);
+    switch(op){
+        case 1:
+            orderAlpha(p,count);
+            break;
+        case 2:
+            orderPrix(p,count);
+            break;
+    }
+    }while(op != 2);
+}
+
 //Function mettre à jour la quantité après avoir introduit le code produit
 void acheterProduit(struct produit p[100], int numTotal){
     int c;
@@ -114,15 +136,13 @@ void acheterProduit(struct produit p[100], int numTotal){
 
 
 }
-//Function l’ordre  décroissant du prix.
-
 int main()
 {
     struct produit ps[100];
     OperationProduits(ps);
-    orderAlpha(ps,2);
-    afficherPreduit(ps,2);
-    orderPrix(ps,2);
-    afficherPreduit(ps,2);
+//    orderAlpha(ps,2);
+//    afficherProduit(ps,2);
+//    orderPrix(ps,2);
+//    afficherProduit(ps,2);
     return 0;
 }
