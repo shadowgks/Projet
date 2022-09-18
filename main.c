9b5fa8c007805469588d318a3a99baadcd53b13c
saad moumou <saad.moumou.coder@gmail.com>
2022-09-18 16:12:17 +0100

    Fix Bugs Function

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 
+int numTotalP = 0;
 //Structures Section
 struct produit{
     //Members
@@ -50,10 +51,10 @@ void OperationProduits(struct produit p[100]){
     }while(OP != 6);
 
 }
-//Function ajouter
+//ajouterProduits
 void ajouterProduits(struct produit p[100],int N){
     //ajouter
-    for(int i=0; i<N; i++){
+    for(int i=numTotalP; i<numTotalP+N; i++){
         printf("\nDonnez Le Code Produit: ");
         scanf("%d",&p[i].code);
         printf("Donnez Le Nom Produit: ");
@@ -64,20 +65,23 @@ void ajouterProduits(struct produit p[100],int N){
         scanf("%f",&p[i].prix);
         p[i].prix += 0.15 * p[i].prix; //TTC 15%
     }
+    numTotalP += N;
 }
 //afficherProduit
-void afficherProduit(struct produit p[100],int N){
+void afficherProduit(struct produit p[100]){
     //afficher
-    for(int j=0; j<N; j++){
-        printf("Produit %d:\nCode: %d\nNom: %s\nQuantitie: %d\nPrix: %.2f\n",j+1,p[j].code,p[j].nom,p[j].quantite,p[j].prix);
+    printf("\t\t\t\tPorduit\t\tCode\tNom\tQuantitie\tPrix\n");
+    printf("\t\t\t\t------------------------------------------------------------\n");
+    for(int j=0; j<numTotalP; j++){
+        printf("\t\t\t\t%d\t\t%d\t%s\t%d\t\t%.2f\n",j+1,p[j].code,p[j].nom,p[j].quantite,p[j].prix);
     }
 }
 //Function l’ordre alphabétique croissant du nom.
-void orderAlpha(struct produit p[100],int count){
+void orderAlpha(struct produit p[100]){
     int i,j;
     struct produit X;
-    for(i=0; i<count-1; i++){
-        for(j=i+1; j<count; j++){
+    for(i=0; i<numTotalP-1; i++){
+        for(j=i+1; j<numTotalP; j++){
             if(strcmp(p[i].nom,p[j].nom) > 0){ //if p[i].nom > p[j].nom So strcmp return value positive
                 X = p[i];
                 p[i] = p[j];
@@ -86,12 +90,12 @@ void orderAlpha(struct produit p[100],int count){
         }
     }
 }
-//Function l’ordre  décroissant du prix.
-void orderPrix(struct produit p[100],int count){
+//l’ordre  décroissant du prix. //Triage par pivot
+void orderPrix(struct produit p[100]){
     int i,j;
     struct produit X;
-    for(i=0; i<count-1; i++){
-        for(j=i+1; j<count; j++){
+    for(i=0; i<numTotalP-1; i++){
+        for(j=i+1; j<numTotalP; j++){
             if(p[i].prix < p[j].prix){
                 X = p[i];
                 p[i] = p[j];
@@ -100,10 +104,9 @@ void orderPrix(struct produit p[100],int count){
         }
     }
 }
-//Function operationTrier
+//operationTrier
 void operationTrier(struct produit p[100]){
     int op;
-    int count = 2;
     do{
     printf("1 - L’ordre Alphabetique Croissant Du Nom:\n");
     printf("2 - L’ordre Decroissant Du Prix:\n");
@@ -111,12 +114,12 @@ void operationTrier(struct produit p[100]){
     scanf("%d",&op);
     switch(op){
         case 1:
-            orderAlpha(p,count);
-            afficherProduit(p,count);
+            orderAlpha(p);
+            afficherProduit(p);
             break;
         case 2:
-            orderPrix(p,count);
-            afficherProduit(p,count);
+            orderPrix(p);
+            afficherProduit(p);
             break;
         case 3:
             system("cls");
@@ -150,16 +153,40 @@ void acheterProduit(struct produit p[100], int numTotal){
             printf("Le code N'existe pas dans la liste des produit ou La Quantite n'est pas suffisante");
        }
     }
-
-
 }
+////Recherche les Produits
+//void rechercheProduits(struct produit p[100]){
+//}
+
+//Function codeP
+void codeP(struct produit p[100], int numTotalP, double c){
+    int i;
+    printf("Donnez le code Produit: ");
+    scanf("%d",&c);
+    for(i=0; i<numTotalP; i++){
+        if(c == p[i].code){
+            printf("Code: %d\nNom: %s\nQuantitie: %d\nPrix: %.2f\n",p[i].code,p[i].nom,p[i].quantite,p[i].prix);
+            break;
+        }
+    }
+}
+//Function quantiteP
+void quantiteP(struct produit p[100], int numTotalP, double q){
+    int i;
+    printf("Donnez la Quantite Produit: ");
+    scanf("%d",&q);
+    for(i=0; i<numTotalP; i++){
+        if(q == p[i].quantite){
+            printf("Code: %d\nNom: %s\nQuantitie: %d\nPrix: %.2f\n",p[i].code,p[i].nom,p[i].quantite,p[i].prix);
+            break;
+        }
+    }
+}
+//void Etat du stock
+
 int main()
 {
     struct produit ps[100];
     OperationProduits(ps);
-//    orderAlpha(ps,2);
-//    afficherProduit(ps,2);
-//    orderPrix(ps,2);
-//    afficherProduit(ps,2);
     return 0;
 }
